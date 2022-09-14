0e9f7e8d4131f4ed7cff210b3c1254d615f0869c
Jinlong Ru <jinlongru@gmail.com>
2022-09-14 15:43:29 +0200

    use TSE

---
+++ R/utils.R
@@ -0,0 +1,78 @@
+library(tidyverse)
+library(data.table)
+
+read_CheckV <- function(fpath) {
+  dtbl <- fread(fpath) %>%
+    mutate(checkv_quality = factor(checkv_quality, levels=c("Complete", "High-quality", "Medium-quality", "Low-quality", "Not-determined"))) %>%
+    column_to_rownames("contig_id") %>%
+    setnames(colnames(.), paste0("checkv_", colnames(.))) %>%
+    setnames("checkv_checkv_quality", "checkv_quality") %>%
+    rownames_to_column("Contig")
+  return(dtbl)
+}
+
+
+read_taxonomy <- function(fpath) {
+  df <- fread(fpath) %>% 
+    mutate(Contig=str_replace(contig_id, "-cat_[1-6]", "")) %>% 
+    mutate(virsorter_category=as.integer(str_replace(contig_id, ".*-cat_", "")))
+    # setnames("length", "viral_length")
+  return(df)
+}
+
+read_VirSorter2 <- function(fpath) {
+  dtbl <- fread(fpath) %>%
+    column_to_rownames("seqname") %>%
+    setnames(colnames(.), paste0("virsorter2_", colnames(.))) %>%
+    rownames_to_column("Contig")
+  return(dtbl)
+}
+
+read_vContact2 <- function(fpath, assembler_label="_NODE_") {
+  df <- fread(fpath) %>% 
+    dplyr::select(-"V1") %>% 
+    filter(str_detect(Genome, assembler_label)) %>% 
+    column_to_rownames("Genome") %>% 
+    setnames(colnames(.), paste0("vc_", str_replace_all(colnames(.), " ", "_"))) %>% 
+    rownames_to_column("contig_id")
+  return(df)
+}
+
+
+read_bins <- function(fbin, fbin_classify) {
+  bin_classify <- fread(fbin_classify) %>% 
+    column_to_rownames("binname") %>% 
+    setnames(colnames(.), paste0("bin_", colnames(.))) %>% 
+    rownames_to_column("binname")
+  bins <- fread(fbin, header = FALSE, col.names = c("binname", "Contig")) %>% 
+    left_join(bin_classify, by = "binname")
+  return(bins)
+}
+
+
+read_coverm <- function(fpath_abundance, sample_metadata) {
+  df_abundance <- fread(fpath_abundance) %>%
+    column_to_rownames("Contig")
+  
+  # remove samples in sample_metadata but not in df_abundance
+  smeta_selected <- sample_metadata %>%
+    dplyr::filter(sample_id %in% colnames(df_abundance))
+
+  # ensure the order of samples in df_abundance is the same as in sample_metadata
+  df2 <- as.matrix(df_abundance[, smeta_selected$sample_id])
+
+  return(df2)
+}
+
+
+refind_abundance <- function(tse, raw_abundance_name, covfrac_name, thr=0.5) {
+  df_abundance_raw <- assay(tse, raw_abundance_name)
+  df_filter <- assay(tse, covfrac_name)
+  df_filter[df_filter<thr] <- 0
+  df_filter[df_filter!=0] <- 1
+  df_abundance_new <- df_abundance_raw * df_filter
+  
+  tse_new <- tse
+  assay(tse_new, raw_abundance_name) <- df_abundance_new
+  return(tse_new)
+}
--- app.R
+++ app.R
@@ -1,47 +1,247 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(here)
+library(shinyWidgets)
+library(tidyverse)
+library(reactable)
+library(plotly)
+library(scales)
+library(shinythemes)
+library(microbiome)
+library(ggpubr)
+library(miaViz)
+library(mia)
+library(scater)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+Sys.setenv("VROOM_CONNECTION_SIZE" = 131072 * 100)
+source(here("R/utils.R"))
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# Functions
+plot_beta_diversity <- function(tse_obj, name, NMDS=FALSE, scale = F, ...) {
+  if (NMDS == TRUE) {
+    tse_obj <- runNMDS(tse_obj, FUN = vegan::vegdist, name = name, ...)
+    xlab <- "Axis 1"
+    ylab <- "Axis 2"
+  } else {
+    tse_obj <- runMDS(tse_obj, FUN = vegan::vegdist, name = name, ...)
+    e <- attr(reducedDim(tse_obj, name), "eig")
+    rel_eig <- 100 * e/sum(e[e>0])
+    xlab <- paste("Axis 1 (", round(rel_eig[[1]], 2), "%)", sep = "")
+    ylab <- paste("Axis 2 (", round(rel_eig[[2]], 2), "%)", sep = "")
+  }
+  p <- plotReducedDim(tse_obj, name, colour_by = "condition") +
+    xlab(xlab) +
+    ylab(ylab) +
+    stat_ellipse(geom = "polygon", alpha=0.1, aes(fill=colour_by), linetype=2) +
+    ggtitle(name) +
+    theme_bw()
+  return(p)
+}
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+# ========= sidebar ==========
+sideP <- sidebarPanel(
+  fileInput(inputId = "file1", label = "Upload viroprofiler_output.RData", multiple = FALSE, accept = c(".RData")),
+  numericInput("min_ctglen", "Minimum contig length:", value = 10000, step = 1000),
+  selectInput("abdc_metric", "Abundance metric:", choices = c("read counts", "trimmed mean"), selected = "read counts"),
+  sliderInput("min_covfrac", "Min Coverage Fraction:", min = 0, max = 1, value = 0.5, step = 0.05, ticks = T),
+  selectInput("taxa_rank", "Taxonomy rank", choices = c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"), selected = "Family"),
+  uiOutput("transform_on"),
+  uiOutput("transform_method"),
+  uiOutput("ctg_feature_col"),
+  fluidRow(column(6, selectInput("checkv_quality", "CheckV quality", choices = c("Complete", "High-quality", "Medium-quality", "Low-quality", "Not-determined"), selected = c("Complete", "High-quality", "Medium-quality", "Low-quality"), multiple = TRUE)),
+           column(2, radioButtons("filter_logic", "Logic", choices = c("AND", "OR"), selected = "AND")),
+           column(4, selectInput("vs2_category", "VirSorter2 category", choices = c(1,2,3,4,5,6), selected = c(1,2,4,5), multiple = TRUE))),
+  numericInput("abundance_min_threshold", "Minimum abundance to show", value = 0.001, min=0),
+  selectInput("narm", "Remove NA in glom:", choices = c(TRUE, FALSE), selected = FALSE),
+  span("More information on ViroProfiler is "),
+  a(href="https://github.com/deng-lab/viroprofiler", "on GitHub", target="_blank"),
+  br(),
+  span("Source code of this app is available "),
+  a(href="https://github.com/deng-lab/viroprofiler-viewer", "here", target="_blank"),
+  width = 3
 )
 
-# Define server logic required to draw a histogram
+# ========= main panel ==========
+mainP <- mainPanel(
+  tabsetPanel(
+    tabPanel("Data overview",
+             fluidRow(column(12, reactableOutput("tbl_rowdata"))),
+             fluidRow(column(6, plotlyOutput("plt_checkv_qc")),
+                      column(6, plotlyOutput("plt_completeness")),
+                      column(6, plotlyOutput("plt_adiversity")),
+                      column(6, plotlyOutput("plt_bdiversity")),
+                      ),
+             ),
+    tabPanel("Assay",
+             fluidRow(column(12, reactableOutput("tbl_abundance"))),
+             fluidRow(column(12, uiOutput("dl_button"))),
+             fluidRow(column(12, plotlyOutput("plt_abundance_barplot"))),
+             ),
+    tabPanel("Metadata", reactableOutput("tbl_smeta"),
+             fluidRow(column(12, reactableOutput("tbl_smeta2")))
+             ),
+    tabPanel("Gene annotation",
+             fluidRow(column(12, reactableOutput("tbl_geneanno"))),
+             fluidRow(column(6, plotlyOutput("plt_AMG")),
+                      column(6, plotlyOutput("plt_PFAM"))),
+             fluidRow(column(6, plotlyOutput("plt_CARD")),
+                      column(6, plotlyOutput("plt_VF"))),
+             ),
+  ),
+  width = 9
+)
+
+navp_raw <- tabPanel("Overview", sidebarLayout(sideP, mainP))
+navp_tutorial <- tabPanel("Tutorial", fixedPage(withMathJax(includeMarkdown("www/tutorial.md")), hr(), div(class="footer", includeHTML("www/footer.html"))))
+ui <- navbarPage("ViroProfiler-viewer", navp_raw, navp_tutorial, theme = shinytheme("united"))
+
+# load("viroprofiler_output.RData")
+# ========= server =================
 server <- function(input, output) {
+    tse_raw <- reactive({
+        if (is.null(input$file1)) {
+            return(NULL)
+        } else {
+            load(input$file1$datapath)
+            feature_anno <- rowData(tse)
+            if (input$filter_logic == "AND") {
+                tse_sel <- tse[feature_anno$checkv_quality %in% input$checkv_quality &
+                               feature_anno$vs2_category %in% input$vs2_category, ]
+            } else {
+                tse_sel <- tse[feature_anno$checkv_quality %in% input$checkv_quality |
+                               feature_anno$vs2_category %in% input$vs2_category, ]
+            }
+        }})
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    abdc_metric <- reactive({
+        if (input$abdc_metric == "read counts") {
+            return("counts")
+        } else if (input$abdc_metric == "trimmed mean") {
+            return("trimmed_mean")
+        }
+    })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    tse <- reactive({
+        tse <- tse_raw()
+        tse <- tse[rowData(tse)$checkv_contig_length>input$min_ctglen,]
+        tse <- refind_abundance(tse, abdc_metric(), "covfrac", input$min_covfrac)
+        # tse <- tse[rowData()]
+    })
+
+
+    sample_meta <- reactive({
+        colData(tse()) %>% 
+            as.data.frame() %>%
+            mutate(across(where(is.numeric), round, 4))
+    })
+
+    feature_meta <- reactive({
+        rowData(tse()) %>% as.data.frame()
+    })
+
+    output$tbl_smeta <- renderReactable({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            sample_meta() %>%
+              reactable(
+                fullWidth = FALSE,
+                wrap = FALSE,
+                resizable = TRUE,
+                striped = TRUE,
+                highlight = TRUE,
+                bordered = TRUE,
+              )
+        }
+    })
+
+    # ========= Data overview ==========
+    output$tbl_rowdata <- renderReactable({
+      if (is.null(tse_raw())) {
+        return(NULL)
+      } else {
+        feature_meta() %>% 
+          reactable(wrap = F, resizable = T)
+      }
+    })
+
+
+    output$plt_checkv_qc <- renderPlotly({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            feature_meta() %>%
+                # dplyr::filter(!checkv_quality %in% c("Not-determined", "Low-quality")) %>% 
+                ggplot(aes(x=checkv_contig_length, fill=checkv_quality)) +
+                geom_histogram(alpha=0.8)
+        }
+    })
+
+    output$plt_completeness <- renderPlotly({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            feature_meta() %>%
+                ggplot(aes(x=checkv_completeness, fill=checkv_quality)) +
+                geom_histogram(alpha=0.8)
+        }
+    })
+
+    output$plt_adiversity <- renderPlotly({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            plotColData(tse(), "richness_observed", "condition", colour_by = "condition")
+        }
+    })
+
+    output$plt_bdiversity <- renderPlotly({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            plot_beta_diversity(tse(), name = "Beta-diversity", method = "bray", exprs_values = "relabundance", ntop=nrow(tse()), NMDS = T)
+        }
+    })
+
+    # ========== Abundance table ==========
+    output$tbl_abundance <- renderReactable({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+              assay(tse(), abdc_metric()) %>%
+                as.data.frame() %>%
+                rownames_to_column("feature") %>%
+                mutate(across(where(is.numeric), round, 4)) %>%
+                reactable(
+                  fullWidth = FALSE,
+                  wrap = FALSE,
+                  resizable = TRUE,
+                  striped = TRUE,
+                  highlight = TRUE,
+                  elementId = "table_abundance",
+                  columns = list(feature = colDef(width = 350)))
+        }
+    })
+    
+    output$dl_button <- renderUI({
+      if (is.null(tse_raw())) {
+        return(NULL)
+      } else {
+      htmltools::browsable(
+        tagList(
+          tags$button(
+            tagList(fontawesome::fa("download"), "Download as CSV"),
+            onclick = "Reactable.downloadDataCSV('table_abundance', 'abundance.csv')"
+          )))
+      }
+    })
+
+    output$plt_abundance_barplot <- renderPlotly({
+        if (is.null(tse_raw())) {
+            return(NULL)
+        } else {
+            plotAbundance(tse(), abund_values=abdc_metric(), rank = input$taxa_rank, use_relative=F)
+        }
     })
 }
 
